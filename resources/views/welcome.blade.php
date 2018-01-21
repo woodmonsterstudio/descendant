e7a365d89ffb892e4d2b78d4bbfbe8da67785f2e
cheehau <woodmonsterstudio@gmail.com>
2018-01-21 13:04:07 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Wahah
                 </div>
 
                 <div class="links">

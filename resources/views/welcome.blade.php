0c9eadb162adecdc11dc5aa667860b1ece8a4990
cheehau <woodmonsterstudio@gmail.com>
2018-01-28 17:49:20 +0800

    draft_done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,6 +51,9 @@
             <li class="nav-item">
               <a class="nav-link" href="{{route('login',['demo'=>'true'])}}">Demo</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/woodmonsterstudio/descendant">GitHub</a>
+            </li>
             @endif
             @if(env('APP_ENV') == 'production')
             <li class="nav-item">
